f2cac5489e2373b4a9a6a0a708ea2445b645b1f1
Fedor Kalin <kalinfed2000@gmail.com>
2025-10-02 11:36:16 +0300

    Add comments to L1.1

--- L1/L1.1/L1.1.go
+++ L1/L1.1/L1.1.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//создание стуктуры
 type Human struct {
 	age     int
 	height  int
@@ -10,6 +11,7 @@ type Human struct {
 	name    string
 }
 
+//создание методов структуры Human
 func (h *Human) GetAge() int {
 	return h.age
 }
@@ -30,14 +32,15 @@ func (h *Human) GetName() string {
 	return h.name
 }
 
+//создание структуры Action с использованием композиции embedded struct(делаем так, чтобы структура Action имела все все методы Human)
 type Action struct {
 	Human
 	action string
 }
 
 func main() {
-	personActivity := Action{
-		Human: Human{
+	personActivity := Action{ //заполняем поля структуры Action
+		Human: Human{ //заполняем поля структуры Human
 			age:     17,
 			height:  187,
 			weight:  77,
@@ -46,7 +49,7 @@ func main() {
 		},
 		action: "is playing footbal now",
 	}
-
+	//проверям, имеет ли структура Action все поля структуры Human
 	fmt.Printf("%s %s. He is %v years old. His weight: %v, his height: %v. He is from %s", personActivity.name, personActivity.action, personActivity.age, personActivity.weight, personActivity.height, personActivity.country)
-
+	//на выходе получаем нужный результат, который подтверждает, что структура Action имеет доступ к методам Human
 }
