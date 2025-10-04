da9d766b430c08a43610c255cbb3bd796cbf78a4
Fedor Kalin <kalinfed2000@gmail.com>
2025-10-04 11:30:56 +0300

    Add comments to L1.5

--- L1/L1.5/L1.5.go
+++ L1/L1.5/L1.5.go
@@ -6,28 +6,38 @@ import (
 )
 
 func main() {
+	//создаем канал
 	ch := make(chan int)
 
+	//задаем время работы программы
 	var n int
 	fmt.Print("Введите время работы программы(сек):")
 	fmt.Scan(&n)
+	//задаем таймаут(сработает через n секунд)
 	timeout := time.After(time.Duration(n) * time.Second)
 
+	//запускаем горутину для обработки числа
 	go analyseNum(ch)
 
+	//запускаем бесконечный цикл
 	for i := 0; ; i++ {
 		select {
+		//если сработал наш таймаут, закрываем канал и пишем что программа
+		//остановлена по истечении n секунд
 		case <-timeout:
 			close(ch)
 			fmt.Printf("Программа остановлена по истечениии %v сек", n)
 			return
+		//пока таймаут не сработал, отправляем числа в канал
 		case ch <- i:
 			time.Sleep(1 * time.Second)
 		}
 	}
 }
 
+// функция принимает на вход канал чтения
 func analyseNum(nums <-chan int) {
+	//читаем числа из канала и печатаем их
 	for num := range nums {
 		fmt.Printf("Число %v обработано\n", num)
 	}
