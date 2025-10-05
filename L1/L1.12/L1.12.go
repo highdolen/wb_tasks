a3c718a7a57091e97fff22fe9671882292d57540
Fedor Kalin <kalinfed2000@gmail.com>
2025-10-05 20:34:13 +0300

    Add comments to L1.12

--- L1/L1.12/L1.12.go
+++ L1/L1.12/L1.12.go
@@ -3,21 +3,29 @@ package main
 import "fmt"
 
 func doSet(str []string) []string {
+	//инициализируем слайс, где будет результирующее множество
 	resultSet := []string{}
+	//инициализируем мапу, где слова будут храниться, как ключ
 	m := make(map[string]struct{})
 
+	//проходимся по нашим словам(слайсу строк)
 	for _, v := range str {
+		//добавляем в мапу слово, как ключ
 		m[v] = struct{}{}
 	}
 
+	//проходимся по мапе
 	for key, _ := range m {
+		//добавляем ключи к нашему результирующему слайсу
 		resultSet = append(resultSet, key)
 	}
 	return resultSet
 }
 
 func main() {
+	//инициализируем слайс слов
 	words := []string{"cat", "cat", "dog", "cat", "tree"}
 
+	//печатаем множество слов
 	fmt.Println(doSet(words))
 }
