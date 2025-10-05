7150a4582fff6862a0cba84ae0b4d727a4c112e3
Fedor Kalin <kalinfed2000@gmail.com>
2025-10-05 20:37:40 +0300

    Add comments to L1.13

--- L1/L1.13/L1.13.go
+++ L1/L1.13/L1.13.go
@@ -3,17 +3,19 @@ package main
 import "fmt"
 
 func changeNums(a, b int) (int, int) {
-	a = a + b
-	b = a - b
-	a = a - b
-	return a, b
+	a = a + b   // 13 + 14 = 27
+	b = a - b   //27-13 = 14
+	a = a - b   //27 - 14 = 13
+	return a, b //возвращаем числа, которые мы поменяли местами(точнее разные стали значения переменных)
 }
 
 func main() {
+	//инициализируем 2 переменные
 	var a, b int
 	fmt.Print("Введите число а: ")
 	fmt.Scan(&a)
 	fmt.Print("Введите число b: ")
 	fmt.Scan(&b)
+	//печатаем числа, которые поменяны местами
 	fmt.Println(changeNums(a, b))
 }
