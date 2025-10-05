6ea6636181edb9dcf9f70429a4089eeab193013a
Fedor Kalin <kalinfed2000@gmail.com>
2025-10-05 22:51:09 +0300

    Add comments to L1.19

--- L1/L1.19/L1.19.go
+++ L1/L1.19/L1.19.go
@@ -3,20 +3,27 @@ package main
 import "fmt"
 
 func reverseWord(str string) string {
+	//переводим строку в руны
 	strRune := []rune(str)
+	//создаем слайс рун(результирующий)
 	resultRune := []rune{}
 
+	//проходимся по слову в обратном порядке
 	for i := len(strRune) - 1; i >= 0; i-- {
+		//добавляем в результирующий слайс руны по порядку
 		resultRune = append(resultRune, strRune[i])
 	}
 
+	//переводим слайс рун в строку и возвращаем ее
 	return string(resultRune)
 }
 
 func main() {
+	//вводим строку
 	fmt.Print("Введите строку: ")
 	var word string
 	fmt.Scan(&word)
 
+	//печатаем перевернутую строку
 	fmt.Println(reverseWord(word))
 }
