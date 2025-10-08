ea91bbd404ffd0eb777e674c57ce0bc8346b5dc0
Fedor Kalin <kalinfed2000@gmail.com>
2025-10-08 19:09:46 +0300

    Add comments to L2.8

--- L2/L2.8/main.go
+++ L2/L2.8/main.go
@@ -1,22 +1,24 @@
 package main
 
+//импортируем нужные библиотеки
 import (
 	"fmt"
 	"os"
 	"time"
 
+	//импортируем библиотеку ntp с github
 	"github.com/beevik/ntp"
 )
 
 func main() {
-	// Получаем текущее время через NTP
+	// получаем текущее время через NTP
 	currentTime, err := ntp.Time("pool.ntp.org")
 	if err != nil {
-		// Ошибка выводится в STDERR
+		// ошибка выводится в STDERR
 		fmt.Fprintln(os.Stderr, "Ошибка получения времени:", err)
 		os.Exit(1)
 	}
 
-	// Выводим время в читаемом формате
+	// выводим время в читаемом формате
 	fmt.Println(currentTime.Format(time.RFC1123))
 }
