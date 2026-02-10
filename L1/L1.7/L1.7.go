35266d7a5cdd624627bbf169d028484eb66b75e7
Fedor Kalin <kalinfed2000@gmail.com>
2026-02-10 22:41:15 +0300

    Change L1.7

--- L1/L1.7/L1.7.go
+++ L1/L1.7/L1.7.go
@@ -5,51 +5,67 @@ import (
 	"sync"
 )
 
+type SafeMap struct {
+	mu sync.Mutex
+	m  map[int]int
+}
+
+// NewSafeMap - конструктор мапы с мьютексом
+func NewSafeMap() *SafeMap {
+	return &SafeMap{
+		m: make(map[int]int),
+	}
+}
+
+// Set - добавление в мапу ключа и значения
+func (s *SafeMap) Set(key int, value int) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	//добавление ключа и значения
+	s.m[key] = value
+
+}
+
+// Print - печать мапы
+func (s *SafeMap) Print() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	fmt.Println(s.m)
+}
+
 func main() {
-	//инициализируем мапу
-	m := make(map[int]int)
+	safeMap := NewSafeMap()
 	//создаем канал
 	ch := make(chan int)
 	//создаем переменную, отвечающую за количество работающих воркеров
 	numWorkers := 3
-	//создаем мюьтекс, для безопасной записи в map
-	mu := sync.Mutex{}
 	//создаем вэйтгруппу, для конкурентной работы
 	wg := &sync.WaitGroup{}
 	//запускаем воркеров
 	for i := 0; i < numWorkers; i++ {
 		//увеличиваем счетчик для каждой задачи
 		wg.Add(1)
-		go worker(ch, i, &mu, wg, m)
+		go worker(ch, i, safeMap, wg)
 	}
 	//запускаем цикл для записи чисел в мапу
 	for i := 7; i < 10; i++ {
 		//передаем число в канал
 		ch <- i
 	}
-
-	//закрываем канал(сообщаем, что задач больше не будет)
 	close(ch)
 
 	//ждем пока работа воркеров завершится
 	wg.Wait()
 	//печатаем мапу
-	fmt.Println(m)
+	safeMap.Print()
 }
 
-// воркер принимает канал чтения, свой айди, мьютекс, вэйтгруппу и мапу(передаем без указателя, так как ссылочный тип)
-func worker(jobs <-chan int, id int, mu *sync.Mutex, wg *sync.WaitGroup, m map[int]int) {
+// worker - принимает канал чтения, свой айди, мапу(с мьютексом) и вейтгруппу
+func worker(jobs <-chan int, id int, s *SafeMap, wg *sync.WaitGroup) {
+	defer wg.Done()
 	//читаем данные из канала
 	for job := range jobs {
-		//мьютимся, чтобы все аккуратно записалось(мапа не потокобезопасна)
-		mu.Lock()
-		//сообщаем о том, какой воркер записал число
+		s.Set(id, job)
 		fmt.Printf("Worker %v - %v\n", id, job)
-		//записываем число в мапу
-		m[id] = job
-		//снимаем лок
-		mu.Unlock()
-		//сообщаем вэйтгруппе о том, что задача выполнена
-		wg.Done()
 	}
 }
